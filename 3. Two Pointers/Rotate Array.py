cd7272e0e38d32040edab0acfe1185346b2d0148
Praveen Uppar <praveenuppar718@gmail.com>
2025-08-07 09:22:18 +0530

    two pointer

---
+++ 3. Two Pointers/Rotate Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # handle edge case wheere k > len(nums)
+        k = k % len(nums)
+        nums[:k], nums[k:] = nums[-k:], nums[:-k]
+        # [1,2,3,4],[5,6,7] = [5,6,7],[1,2,3,4]
+        return nums
+        
+        # Eg: [1,2,3,4,5,6,7] k = 3
+        # nums[-3:] becomes [5, 6, 7].
+        # nums[:-3] becomes [1, 2, 3, 4].
+        # nums[:3], nums[3:] = [5, 6, 7], [1, 2, 3, 4] 
