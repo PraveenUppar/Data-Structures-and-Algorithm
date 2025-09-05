e625be1b831b995b06606e636b21e2b817920df1
Praveen Uppar <praveenuppar718@gmail.com>
2025-09-05 12:56:04 +0530

    binary search

--- 7. Binary Search/Search in Rotated Sorted Array.py
+++ 7. Binary Search/Search in Rotated Sorted Array.py
@@ -10,20 +10,17 @@ class Solution:
             if nums[mid] == target:
                 return mid
 
-            if nums[left] <= nums[mid]:  
-                if target > nums[mid]:
-                    left = mid + 1
-                elif nums[left] > target:
-                    left = mid + 1
-                else:
-                    right = mid - 1
-            else:   
-                if target < nums[mid]:
-                    right = mid - 1
-                elif nums[right] < target:
+            elif nums[left] <= nums[mid]:
+                if nums[left] <= target <= nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
+                    
+            else:
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
         return -1
 
         # Brute Force = O(n)
