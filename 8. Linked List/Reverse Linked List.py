391da3a08e079e51d669ab00e484b8c274f0188b
Praveen Uppar <praveenuppar718@gmail.com>
2025-09-08 14:22:36 +0530

    linked list

---
+++ 8. Linked List/Reverse Linked List.py
@@ -0,0 +1,17 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        prev = None
+        curr = head
+
+        while curr: # meaning till curr is not zero or null
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
