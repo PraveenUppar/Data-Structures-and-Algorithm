9dad69415d317a244a09c2aab11af8d58744ba1a
Praveen Uppar <praveenuppar718@gmail.com>
2025-09-19 12:32:38 +0530

    trees

--- 9. Trees/Binary Tree Right Side View.py
+++ 9. Trees/Binary Tree Right Side View.py
@@ -1,25 +1,25 @@
-    # Definition for a binary tree node.
-    class TreeNode:
-        def __init__(self, val=0, left=None, right=None):
-            self.val = val
-            self.left = left
-            self.right = right
-    class Solution:
-        def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
             
-            res = []
+        res = []
 
-            def dfs(root,depth):
+        def dfs(root,depth):
 
-                if not root:
-                    return None
+            if not root:
+                return None
                 
-                if depth == len(res):
-                    res.append(root.val)
+            if depth == len(res):
+                res.append(root.val)
 
-                dfs(root.right, depth + 1)
-                dfs(root.left, depth + 1)
+            dfs(root.right, depth + 1)
+            dfs(root.left, depth + 1)
 
-            dfs(root, 0)
+        dfs(root, 0)
 
-            return res
+        return res
+        
