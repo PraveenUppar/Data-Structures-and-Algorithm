654139ac0d4c49189f52ee7c84f33b6ac5e5eb87
Praveen Uppar <praveenuppar718@gmail.com>
2025-12-27 09:43:38 +0530

    dp

--- 13. DP/Climbing Stairs.py
+++ 13. DP/Climbing Stairs.py
@@ -1,9 +1,31 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
 
-        def dfs(i):
-            if i >= n:
-                return i == n
-            return dfs(i + 1) + dfs(i + 2)
+        # def dfs(i):
+        #     if i >= n:
+        #         return i == n
+        #     return dfs(i + 1) + dfs(i + 2)
 
-        return dfs(0)
+        # return dfs(0)
+
+        # if n <= 2:
+        #     return n
+
+        # a = 1
+        # b = 2
+
+        # for i in range(3, n+1):
+        #     temp = a + b
+        #     a = b
+        #     b = temp
+        # return b
+
+
+        one, two = 1, 1
+
+        for i in range(n - 1):
+            temp = one
+            one = one + two
+            two = temp
+
+        return one
