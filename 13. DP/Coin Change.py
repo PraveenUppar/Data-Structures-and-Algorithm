c296952158fd46d30eecfc75df1069ee747371ce
Praveen Uppar <praveenuppar718@gmail.com>
2026-01-09 12:25:48 +0530

    dp

--- 13. DP/Coin Change.py
+++ 13. DP/Coin Change.py
@@ -1,49 +1,41 @@
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-
+    def change(self, amount: int, coins: List[int]) -> int:
+        
         # Recursion using dfs
-        # def dfs(amount):
+        # def dfs(i, target):
 
-        #     if amount == 0:
+        #     if i >= len(coins) or target < 0:
         #         return 0
             
-        #     res = float("inf")
-
-        #     for coin in coins:
-        #         if amount - coin >= 0:
-        #             res = min(res, 1 + dfs(amount - coin))
+        #     if target == 0:
+        #         return 1
+            
+        #     res = dfs(i + 1, target)
+            
+        #     if target >= coins[i]:
+        #         res += dfs(i, target - coins[i])
         #     return res
 
-        # min_coin = dfs(amount)
+        # return dfs(0, amount)
 
-        # if min_coin >= float("inf"):
-        #     return -1
-        # else:
-        #     return min_coin
+        # Top Down DP
+        memo = {} 
 
-        # DP using memo
-
-        memo = {}
-
-        def dfs(amount):
-
-            if amount == 0:
+        def dfs(i, target):
+            if target == 0:
+                return 1
+            if i == len(coins) or target < 0:
                 return 0
-
-            if amount in memo:
-                return memo[amount]
-
-            res = float("inf")
             
-            for coin in coins:
-                if amount - coin >= 0:
-                    res = min(res, 1 + dfs(amount - coin))
-
-            memo[amount] = res
+            if (i, target) in memo:
+                return memo[(i, target)]
+            
+            res = dfs(i + 1, target)
+            
+            if target >= coins[i]:
+                res += dfs(i, target - coins[i])
+            
+            memo[(i, target)] = res
             return res
 
-        minCoins = dfs(amount)
-        if minCoins >= float("inf"):
-            return -1
-        else:
-            return minCoins
+        return dfs(0, amount)
